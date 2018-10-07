227e9f12ba1809dec0b598a8af2a4549493bab4d
abadawi2 <abadawi@terpmail.umd.edu>
2018-10-07 20:46:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function total() {
 }
 
 function removeFromCart(item) {
-  for (let i = 0, i < cart.length, i++){
+  for (let i = 0; i < cart.length, i++){
     if (cart[i].itemName === item)
     cart.splice (i,1);
     return cart;
