70e72c6fdbc8bc2ac40238a3fcd27ded0961a624
abadawi2 <abadawi@terpmail.umd.edu>
2018-10-07 19:05:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,35 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var newItem= {
+   'itemName': item,
+   'itemPrice': Math.random() * (99 - 0) + 1};
+ cart.push(newItem);
+ return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length===0){
+    return 'Your shopping cart is empty.'
+  }
+  var itemPrice = [];
+  for (var i = 0; cart.length > i; i++){
+    itemPrice.push(`${cart[i].itemName}` +' at $'+ `${cart[i].itemPrice}`);
+  }
+  if (cart.length === 1) {
+    return 'In your cart, you have '+ itemPrice+'.'
+  }
+  else if (cart.length === 2) {
+    return 'In your cart, you have '+itemPrice[0]+', and '+itemPrice[1]+'.'
+  }
+  else {
+  var string = 'In your cart, you have '
+    for (var i = 0; cart.length - 1 > i; i++){
+     string = string + itemPrice[i];
+    }
+    return string + ` and ${itemPrice[itemPrice.length - 1]}.`;
+  }
+
 }
 
 function total() {
