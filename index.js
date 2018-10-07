4a057bc20ec3bdf3b81db0bd697231f71c7ab4b3
abadawi2 <abadawi@terpmail.umd.edu>
2018-10-07 19:37:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function viewCart() {
     }
     else {
       var string = `In your cart, you have `;
-      for (var i = cart.length; i < cart.length-1; i ++){
+      for (var i = cart.length; i < cart.length - 1; i ++){
        string = string + itemPrice[i];
       }
       return string + `and ${itemPrice[itemPrice.length - 1]}.`;
